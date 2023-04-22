c0d3f562c4d8fff004b1dfea86822d770a57d2a0
SincerelyFaust <hedesmarin@gmail.com>
2023-04-22 21:15:22 +0200

    fix(hello): fix res not being a function

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -1,11 +1,14 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
-import type { NextApiResponse } from "next";
+import type { NextApiRequest, NextApiResponse } from "next";
 
 type Data = {
   name: string;
 };
 
-export default function handler(res: NextApiResponse<Data>) {
+export default function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<Data>,
+) {
   res.status(200).json({ name: "Dobrodošli na Mad Science API :)" });
 }
