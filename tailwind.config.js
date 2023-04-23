6fd863e0a5a79e5b8b5ee504ed957862ee0c8015
SincerelyFaust <hedesmarin@gmail.com>
2023-04-23 16:02:16 +0200

    fix(tailwind): properly set inter as default font

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,5 @@
+const { fontFamily } = require("tailwindcss/defaultTheme");
+
 /** @type {import('tailwindcss').Config} */
 
 module.exports = {
@@ -11,13 +13,28 @@ module.exports = {
   theme: {
     extend: {
       fontFamily: {
-        sans: ["var(--font-inter)"],
+        primary: ["var(--font-inter)", ...fontFamily.sans],
+        serif: ["var(--lora-inter)", ...fontFamily.serif],
       },
       screens: {
         "2xl": { max: "1620px" },
         "xl": { max: "1486px" },
         "lg": { max: "1073px" },
         "sm": { max: "800px" },
+      },
+      keyframes: {
+        overlayShow: {
+          from: { opacity: 0 },
+          to: { opacity: 1 },
+        },
+        contentShow: {
+          from: { opacity: 0, transform: "translate(-50%, -48%) scale(0.96)" },
+          to: { opacity: 1, transform: "translate(-50%, -50%) scale(1)" },
+        },
+      },
+      animation: {
+        overlayShow: "overlayShow 150ms cubic-bezier(0.16, 1, 0.3, 1)",
+        contentShow: "contentShow 150ms cubic-bezier(0.16, 1, 0.3, 1)",
       },
     },
   },
