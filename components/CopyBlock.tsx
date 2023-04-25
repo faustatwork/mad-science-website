4d03b709b7ae075e506b9c67e2e312f96a196159
SincerelyFaust <hedesmarin@gmail.com>
2023-04-25 22:59:57 +0200

    fix(copyblock): add icons

--- components/CopyBlock.tsx
+++ components/CopyBlock.tsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import { Copy, CheckCheck } from "lucide-react";
 
 interface CopyBlockProps {
   value: string;
@@ -25,15 +26,31 @@ export default function CopyBlock({ value }: CopyBlockProps) {
 
   const btnStyle = copied ? "bg-gray-200 dark:bg-gray-800 " : "";
   return (
-    <div className="text-center">
+    <div className="relative text-center">
       <button
         onClick={copyToClipboard}
         className={
           btnStyle +
-          "-mt-8 w-80 truncate rounded border border-gray-300 p-3 text-sm text-black transition hover:bg-gray-300 dark:border-gray-700 dark:text-white dark:hover:bg-gray-700"
+          "-mt-8 w-80 rounded border border-gray-300 bg-white py-3 px-4 text-sm text-black transition hover:bg-gray-300 dark:border-gray-700 dark:bg-zinc-900 dark:text-white dark:hover:bg-gray-700"
         }
       >
-        {copied ? "Kopirano" : value}
+        <p className="overflow-hidden text-clip">
+          {copied ? (
+            <>
+              <span className="mr-2 inline-block align-middle">
+                <CheckCheck size={17} />
+              </span>
+              Kopirano
+            </>
+          ) : (
+            value
+          )}
+        </p>
+        {copied ? null : (
+          <span className="absolute right-2.5 bottom-2.5 bg-inherit pl-2">
+            <Copy className="p-[3px]" />
+          </span>
+        )}
       </button>
     </div>
   );
