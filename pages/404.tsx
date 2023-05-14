d79cc0211df5d5a85e92514b156e31f93536fa9a
SincerelyFaust <hedesmarin@gmail.com>
2023-05-14 23:56:54 +0200

    fix(404): remove unnecessary div, add icon to button

--- pages/404.tsx
+++ pages/404.tsx
@@ -3,6 +3,7 @@ import Head from "next/head";
 import Footer from "../components/Footer";
 import Navbar from "../components/Navbar";
 import Link from "next/link";
+import { ArrowLeft } from "lucide-react";
 
 const Error404: NextPage = () => {
   return (
@@ -17,22 +18,23 @@ const Error404: NextPage = () => {
         <link rel="canonical" href="https://mad-science.io" />
       </Head>
       <Navbar />
-      <div className="flex min-h-screen w-screen flex-col items-center justify-center">
-        <section className="flex w-full flex-1 flex-col items-center justify-center gap-5 px-20 text-center sm:px-10">
-          <h1 className="text-9xl font-black text-gray-200 dark:text-gray-800">
-            404
-          </h1>
-          <p className="text-2xl font-bold tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl">
-            Uh-oh!
-          </p>
-          <p className="text-gray-500 ">Ova stranica ne postoji.</p>
-          <Link
-            href="/"
-            className="inline-block rounded bg-blue-600 px-5 py-3 text-sm font-medium text-white hover:bg-blue-700 focus:ring-4 focus:ring-blue-200 dark:focus:ring-blue-900"
-          >
-            Nazad
-          </Link>
-        </section>
+      <div className="flex min-h-screen w-screen flex-col items-center justify-center gap-5 px-20 text-center sm:px-10">
+        <h1 className="text-9xl font-black text-gray-200 dark:text-gray-800">
+          404
+        </h1>
+        <p className="text-2xl font-bold tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl">
+          Uh-oh!
+        </p>
+        <p className="text-gray-500 ">Ova stranica ne postoji.</p>
+        <Link
+          href="/"
+          className="inline-block rounded bg-blue-600 px-5 py-3 text-sm font-medium text-white hover:bg-blue-700 focus:ring-4 focus:ring-blue-200 dark:focus:ring-blue-900"
+        >
+          <span className="mr-2 inline-block align-middle">
+            <ArrowLeft size={20} />
+          </span>
+          Nazad
+        </Link>
       </div>
       <Footer />
     </>
