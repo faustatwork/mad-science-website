93c9fc2b390c13db8855d3957477c0fd6551d9d3
SincerelyFaust <hedesmarin@gmail.com>
2023-05-22 21:09:41 +0200

    fix(document): add more keywords

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -15,7 +15,7 @@ export default class MadScienceWebsiteDocument extends Document {
           {/* Meta tags */}
           <meta
             name="keywords"
-            content="mad science, mad, science, trading, crypto, financial, financije, ulaganje, investments, investing, investicije, kurs, course, tecaj, zarada, algo concept, mad concept, mad koncept, algo koncept, profit, balkan, uspjeh, forex, novac"
+            content="mad science, mad, trading grupa, trejding grupa, science, trading, crypto, financial, financije, ulaganje, investments, investing, investicije, kurs, course, tecaj, zarada, algo concept, mad concept, mad koncept, algo koncept, profit, balkan, uspjeh, forex, novac"
           />
           <meta property="og:image" content="/images/banner.webp" />
           <meta property="og:site_name" content="Mad Science" />
