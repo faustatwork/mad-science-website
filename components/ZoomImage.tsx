d247b0a866ae55080a5db75c586ce12d17a83dfc
SincerelyFaust <hedesmarin@gmail.com>
2023-05-30 00:05:47 +0200

    feat(images): add images section

---
+++ components/ZoomImage.tsx
@@ -0,0 +1,32 @@
+import Image, { StaticImageData } from "next/image";
+import Zoom from "react-medium-image-zoom";
+
+interface ZoomImageProps {
+  images: StaticImageData[];
+}
+
+const ZoomImage = ({ images }: ZoomImageProps) => {
+  return (
+    <>
+      <div className="flex h-fit w-full flex-row flex-wrap items-center justify-center gap-5">
+        {images.map(image => {
+          return (
+            <>
+              <Zoom>
+                <Image
+                  alt="image"
+                  src={image}
+                  priority
+                  quality={100}
+                  className="min-w-[8rem] max-w-[16rem] rounded-md transition duration-200 ease-in hover:scale-105 sm:max-w-[5rem]"
+                />
+              </Zoom>
+            </>
+          );
+        })}
+      </div>
+    </>
+  );
+};
+
+export default ZoomImage;
