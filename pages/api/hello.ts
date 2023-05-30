115d6dcf88a4824eda409ce5b16a994bc2960448
SincerelyFaust <hedesmarin@gmail.com>
2023-05-30 18:30:39 +0200

    fix(api/hello): rename object property

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -1,14 +1,12 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-
 import type { NextApiRequest, NextApiResponse } from "next";
 
 type Data = {
-  name: string;
+  data: string;
 };
 
 export default function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>,
 ) {
-  res.status(200).json({ name: "Dobrodošli na Mad Science API :)" });
+  res.status(200).json({ data: "Dobrodošli na Mad Science API :)" });
 }
