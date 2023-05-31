a967bcff3abbf1c3e8c1f11975ed3bc89b5c4bc6
SincerelyFaust <hedesmarin@gmail.com>
2023-05-31 20:18:27 +0200

    fix(zoomimage): adjust border radius

--- components/ZoomImage.tsx
+++ components/ZoomImage.tsx
@@ -18,7 +18,7 @@ const ZoomImage = ({ images }: ZoomImageProps) => {
                   src={image}
                   priority
                   quality={100}
-                  className="max-w-[16rem] rounded-2xl transition duration-200 ease-in hover:scale-105 sm:max-w-[6rem]"
+                  className="max-w-[16rem] rounded-xl transition duration-200 ease-in hover:scale-105 sm:max-w-[6rem]"
                 />
               </Zoom>
             </>
